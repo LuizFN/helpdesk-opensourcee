bb858307c3ab75c05fc735f84a89da20c40ba8a2
Luiz Felipe do Nascimento <fellippe.nascimento@gmail.com>
2021-10-08 15:25:21 -0300

    U

--- helpdesk/Frontend/gerenciarSistemaChamado.php
+++ helpdesk/Frontend/gerenciarSistemaChamado.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Lista Chamado Usuário</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <style>
         body {
@@ -13,108 +13,119 @@
         #dpc {
             background-color: white;
             padding: 20px;
-            width: 1200px;
-            height: 1000px;
-            margin-top: 120px;
+            width: 1400px;
+            height: 500px;
+            margin-top: 40px;
             margin-bottom: 200px;
             box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.616);
         }
-        #filanalista {
+        #tipodemanda {
             height: 400px;
             overflow: auto;
         }
-        #filageral {
+        #status {
             height: 400px;
             overflow: auto;
         }
-        #titulo {
+        #tipoatendimento {
+            height: 400px;
+            overflow: auto;
+        }
+        #bemv {
+            margin-top: 40px;
             text-align: center;
-            border-style: solid;
+            font-weight: bolder;
+            font-size: 25px;
+            font-family: serif;
         }
     </style>
 </head>
 <body>
-    <main class="row justify-content-center align-items-center">
+    <div id="bemv"><p>Olá, nomeUsuario</p></div>
+    <div class="row justify-content-center align-items-center">
         <div id="dpc">
-            <div id="titulo"><h3>MINHA FILA</h3></div>
-            <div id="filanalista">
+            <div id="tipodemanda">
                 <table class="table table-bordered">
                     <thead>
                         <tr>
-                            <th>Chamado</th>
-                            <th>Usuário</th>
-                            <th>Tipo>Categoria>SubCategoria>Item</th> 
-                            <th>Descrição</th>
-                            <th>Status</th>
-                            <th>Data Inicio</th>
-                            <th>Data Fim</th>
-                             <th>Ação</th>
+                            <th>Tipo</th>
+                            <th>Categoria</th>
+                            <th>SubCategoria</th>
+                            <th>Item</th>
+                            <th>Ação</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td>Exemplo</td>
                             <td>Exemplo</td>
-                            <td>Tipo>Categoria>SubCategoria>Item</td>
                             <td>Exemplo</td>
                             <td>Exemplo</td>
-                            <td>Exemplo</td>
-                            <td>Exemplo</td>
-                            <td><a href="verChamadoAnalista.php">Ver</a></td>
+                            <td><a href="verChamadoUsuario.php">Ver</a></td>
                         </tr>
                         <tr>
                             <td>Exemplo</td>
                             <td>Exemplo</td>
-                            <td>Tipo>Categoria>SubCategoria>Item</td>
                             <td>Exemplo</td>
                             <td>Exemplo</td>
-                            <td>Exemplo</td>
-                            <td>Exemplo</td>
-                            <td><a href="verChamadoAnalista.php">Ver</a></td>
+                            <td><a href="verChamadoUsuario.php">Ver</a></td>
                         </tr>
                     </tbody>
                 </table>
-            </div> <!--filanalista-->
-            <div id="titulo"><h3>FILA GERAL</h3></div>
-            <div id="filageral">
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Chamado</th>
-                            <th>Usuário</th>
-                            <th>Tipo>Categoria>SubCategoria>Item</th>
-                            <th>Descrição</th>
-                            <th>Status</th>
-                            <th>Data Inicio</th>
-                            <th>Data Fim</th>
-                             <th>Ação</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Exemplo</td>
-                            <td>Exemplo</td>
-                            <td>Tipo>Categoria>SubCategoria>Item</td>
-                            <td>Exemplo</td>
-                            <td>Exemplo</td>
-                            <td>Exemplo</td>
-                            <td>Exemplo</td>
-                            <td><a href="verChamadoAnalista.php">Ver</a></td>
-                        </tr>
-                        <tr>
-                            <td>Exemplo</td>
-                            <td>Exemplo</td>
-                            <td>Tipo>Categoria>SubCategoria>Item</td>
-                            <td>Exemplo</td>
-                            <td>Exemplo</td>
-                            <td>Exemplo</td>
-                            <td>Exemplo</td>
-                            <td><a href="verChamadoAnalista.php">Ver</a></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div> <!--filageral-->
+            </div> <!--tipodemanda-->
         </div> <!--dpc-->
-    </main> 
+        <div class="row justify-content-center align-items-center">
+        <div id="dpc">
+            <div id="status">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Codigo</th>
+                            <th>Status</th>
+                            <th>Ação</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Exemplo</td>
+                            <td>Exemplo</td>
+                            <td><a href="verChamadoUsuario.php">Ver</a></td>
+                        </tr>
+                        <tr>
+                            <td>Exemplo</td>
+                            <td>Exemplo</td>
+                            <td><a href="verChamadoUsuario.php">Ver</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div> <!--status-->
+        </div> <!--dpc-->
+        <div class="row justify-content-center align-items-center">
+        <div id="dpc">
+            <div id="tipoatendimento">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Codigo</th>
+                            <th>Tipo Atendimento</th>
+                            <th>Ação</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Exemplo</td>
+                            <td>Exemplo</td>
+                            <td><a href="verChamadoUsuario.php">Ver</a></td>
+                        </tr>
+                        <tr>
+                            <td>Exemplo</td>
+                            <td>Exemplo</td>
+                            <td><a href="verChamadoUsuario.php">Ver</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div> <!--tipoatendimento-->
+        </div> <!--dpc-->
+    </div> 
 </body>
 </html>
