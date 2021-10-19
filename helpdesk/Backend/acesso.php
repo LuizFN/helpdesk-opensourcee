73982d715e37d97640cb5a88bf1316a7a4e7c55f
Luiz Felipe do Nascimento <fellippe.nascimento@gmail.com>
2021-10-19 09:08:52 -0300

    Update acesso.php

--- helpdesk/Backend/acesso.php
+++ helpdesk/Backend/acesso.php
@@ -3,12 +3,11 @@
     session_start();
 
     if(isset($_SESSION['usuario']) && is_array($_SESSION['usuario'])) {
-        $nivel = $_SESSION['nivel'][1];
-        $matricula = $_SESSION['matricula'][0];
+        $nivel = $_SESSION['usuario'][1];
+        $matricula = $_SESSION['usuario'][0];
     } else {
         header("location: ../index.php");
     }
-    echo "$nivel";
     if($nivel == 0) {
         header("location: ../Frontend/listaChamadoUsuario.php");
     } else if($nivel == 1) {
